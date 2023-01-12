7a72b6a9f2973a7d3a5c95f49724cb6e5440f56f
Matt Saqueli <msaqueli22@turing.edu>
2023-01-12 15:35:57 -0600

    Add fixes to cleanUp

--- cleanUp.js
+++ cleanUp.js
@@ -8,6 +8,7 @@
 
 
 // EX 1:
+// Added semicolons to where it was needed, thats the standard.
 function nameQuestion() {
   console.log("Hello, what is your name?");
 }
@@ -16,6 +17,7 @@ nameQuestion();
 
 
 // EX 2:
+// Added semicolons and fixed the positioning of the last curly bracket.
 function addThreeNums(first, second, third) {
   var sum = first + second + third;
   console.log(sum);
@@ -24,8 +26,8 @@ function addThreeNums(first, second, third) {
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
-
 // EX 3:
+//Corrected the word 'function' and the position of the last curly bracket.
 function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
@@ -38,6 +40,7 @@ makeFreshPesto();
 
 
 //  EX 4:
+// Added numbers to the call. Added a starting curly bracket in line 44. Put console.log into the code block. 
 function average(num1, num2) {
   var sum = num1 + num2;
   var average = sum / 2;
