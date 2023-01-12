7914bca40f602a0b06c69b669c09540cefecc25e
Matt Saqueli <msaqueli22@turing.edu>
2023-01-12 13:55:46 -0600

    Add answers to cleanUp

--- cleanUp.js
+++ cleanUp.js
@@ -9,38 +9,38 @@
 
 // EX 1:
 function nameQuestion() {
-  console.log("Hello, what is your name?")
+  console.log("Hello, what is your name?");
 }
 
-nameQuestion()
+nameQuestion();
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-console.log(sum);
-      }
+  var sum = first + second + third;
+  console.log(sum);
+}
 
 addThreeNums(1, 2, 3);
 addThreeNums(4, 2, 7);
 
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
-
-  console.log(`the average is: ${avg}`.)
-  }
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var average = sum / 2;
+  console.log(`the average is: ${average}.`);
+}
+ average();
