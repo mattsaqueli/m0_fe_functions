ead6e792e10bb25abc773c6114403f448dcb38a8
Matt Saqueli <msaqueli22@turing.edu>
2023-01-12 14:51:42 -0600

    Add corrections to 3

--- functions.js
+++ functions.js
@@ -18,26 +18,35 @@ greetUser("Matt", "Saqueli");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
 
-function subtract(num1, num2) {
-    var sum = num1, num2;
-    return sum;
+function payRange(companyName, minPay, maxPay) {
+    var range = (maxPay - minPay)
+    console.log(`The pay range for ${companyName} is ${range}.`);
 }   
-    console.log(`Java Company's pay range is ${subtract(90000, 100000)}.`)
      
-
-subtract();
+payRange("Amazon", 50000, 90000)
 
 // 4: Write a function that satifies the following interaction pattern:
 
-checkStock(4, "Coffee");
+checkStock(4, "Coffee")
 
 // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
+checkStock(3, "Tortillas")
+
 // => "Tortillas - running LOW"
 
-checkStock(0, "Cheese");
+checkStock(0, "Cheese")
+
 // => "Cheese - OUT of stock!"
 
-checkStock(1, "Salsa");
+checkStock(1, "Salsa")
 // => "Salsa - running LOW"
+
+function checkStock(itemAmount, item){
+   if (itemAmount === 0) {
+        console.log(`${item} - OUT of stock!`)  
+}  else if (itemAmount < 4) {
+        console.log(`${item} - running LOW`)
+}  else {
+        console.log(`${item} is stocked`)
+  }}
